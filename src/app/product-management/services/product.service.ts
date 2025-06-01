b34904590c817733fea88451a2c0f4eec683de1e
somai hamza <HAMZASOMAI@outlook.com>
2025-06-01 16:05:07 +0100

    commit: add_edit_delete_popup_porduct

--- src/app/product-management/services/product.service.ts
+++ src/app/product-management/services/product.service.ts
@@ -7,7 +7,7 @@ import { Observable, of } from 'rxjs';
 })
 export class ProductService {
 
-private products: Product[] = [
+  private products: Product[] = [
     {
       ProductId: 1,
       productName: 'Laptop Dell',
@@ -22,12 +22,26 @@ private products: Product[] = [
     }
   ];
   constructor() { }
-  
-  getAll():Observable<Product[]>{
-      return of(this.products);
+
+  getAll(): Observable<Product[]> {
+    return of(this.products);
   }
 
-  create(product: Product){
+  create(product: Product): Observable<void> {
+    product.ProductId = Math.max(...this.products.map(x=>x.ProductId))+1;
     this.products.push(product);
+    return of();
+  }
+
+  update(product: Product): Observable<void> {
+    const index = this.products.findIndex(p => p.ProductId === product.ProductId);
+    if (index !== -1) {
+      this.products[index] = product;
+    }
+    return of();
+  }
+
+  delete(id: number) {
+    this.products = this.products.filter(x => x.ProductId !== id);
   }
 }
