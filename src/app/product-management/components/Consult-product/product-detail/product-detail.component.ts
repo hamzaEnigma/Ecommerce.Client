a408e569b7fa31135266b5279faa8581509665e0
somai hamza <HAMZASOMAI@outlook.com>
2025-06-29 13:47:25 +0200

    commit: ajouter bouton ajouter au panier

--- src/app/product-management/components/Consult-product/product-detail/product-detail.component.ts
+++ src/app/product-management/components/Consult-product/product-detail/product-detail.component.ts
@@ -9,5 +9,15 @@ import { Product } from '../../../models/product.model';
 })
 export class ProductDetailComponent {
     @Input() selectedProduct:Product | null = null;
+quantity: number = 1;
+addedToCart: boolean = false;
 
+addToCart(): void {
+  // Ici tu peux appeler un service Panier, par exemple
+  console.log(`Produit ID ${this.selectedProduct?.productId} ajouté avec quantité ${this.quantity}`);
+  this.addedToCart = true;
+
+  // Optionnel : réinitialiser le message après 2s
+  setTimeout(() => this.addedToCart = false, 2000);
+}
 }
